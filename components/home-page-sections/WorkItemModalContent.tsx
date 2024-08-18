08d7788b8650fe6c7cfe7979ee10e7aeba896615
hamid <->
2024-08-18 21:35:14 +0330

    fix: gallery and modal close

--- components/home-page-sections/WorkItemModalContent.tsx
+++ components/home-page-sections/WorkItemModalContent.tsx
@@ -49,9 +49,9 @@ function Tools({ tools }) {
     <div>
       <h4 className="H4 c-base11">Technologies Used</h4>
       <div className="h-3"></div>
-      <ul className="flex gap-1.5">
+      <ul className="flex gap-1.5 flex-wrap">
         {tools.map((t, index) => {
-          return <li key={`technologies-used-${index}`} className="rd-full b-1 b-base6  px-2.5 fs-xs ">{t}</li>;
+          return <li key={`technologies-used-${index}`} className="rd-full b-1 b-base6  px-2.5 fs-xs whitespace-nowrap">{t}</li>;
         })}
       </ul>
     </div>
