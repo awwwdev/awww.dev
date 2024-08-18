c4a44ca81a90a40179b45aafdbb69cb7960dc8b9
hamid <->
2024-08-18 20:33:52 +0330

    fix: pointer styles

--- components/home-page-sections/MouseCursor.tsx
+++ components/home-page-sections/MouseCursor.tsx
@@ -7,7 +7,7 @@ export default function MouseCursor() {
     <span className="relative ">
       <motion.span
         role="none"
-        className="absolute right-0 bottom-0  leading-1  w-0.6em h-0.6em c-blue12A  "
+        className="absolute right-0 bottom-0  leading-1  w-0.6em h-0.6em c-cyan12 drop-shadow-2xl "
         animate={["initial"]}
         variants={{
           initial: {
