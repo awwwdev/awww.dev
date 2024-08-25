ebfb5b25fd74d97d73d9d3c14b9edafcbe7168c7
hamid <->
2024-08-25 09:05:54 +0330

    feat: update Noise

--- components/ui/Noise/Noise.tsx
+++ components/ui/Noise/Noise.tsx
@@ -8,15 +8,14 @@ export type NoiseProps = {
   numOctaves?: string
 } 
 
-export default function Noise({opacity = '0.12', frequancy= '1.2', numOctaves = '6' , style = {} , className = ""}) {
+export default function Noise({ frequancy= '1.2', numOctaves = '6' , style = {} , className = ""}) {
   return (
     <div
       style={{
-        backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg' width='200px' height='200px'  %3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='${frequancy}' numOctaves='${numOctaves}' stitchTiles='stitch' /%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' /%3E%3C/svg%3E")`,
+        backgroundImage: `url("data:image/svg+xml,%3Csvg style='opacity:0.12;' viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg' width='200px' height='200px'  %3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='${frequancy}' numOctaves='${numOctaves}' stitchTiles='stitch' /%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' /%3E%3C/svg%3E")`,
         backgroundRepeat: 'repeat',
         backgroundSize: 'auto',
         filter: 'grayscale(1)',
-        opacity,
         ...style,
       }}
       className={className}
