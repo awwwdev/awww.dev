9b2e272986cfeb05a3d74b625cbd373be978da00
hamid <->
2024-08-27 10:02:35 +0330

    fix: disable lightningcss for now

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@
 const nextConfig = {
   // reactStrictMode: true,
   swcMinify: true,
+  // experimental: {
+  //   useLightningcss: true,
+  // },
   typescript: {
     ignoreBuildErrors: true,
   },
