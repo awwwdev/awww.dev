7f59491df53645f595a85d9fa445df345ef1ab63
hamid <->
2024-08-27 19:12:03 +0330

    fix: tool styles

--- components/home-page-sections/Tools.tsx
+++ components/home-page-sections/Tools.tsx
@@ -61,7 +61,7 @@ export default function Tools() {
                 </>
               }
               category="painful"
-              tooltipContent={<p>Painful, But Life Saving</p>}
+              tooltipContent={<p>Painful, But Life Saving!</p>}
               glowingCategory={glowingCategory}
               isToolTipOpen={isToolitip3Open}
               setIsTooltipOpen={setIsTooltip3Open}
@@ -73,8 +73,12 @@ export default function Tools() {
 
         <ul className="toolkit__grid">
           <Li
-            className=" bg-gradient-to-br from-purple1A via-25%  to-mauve1A via-transparent c-white"
+            className="  c-white"
+            bgGradient="!bg-gradient-to-br from-purple1A via-25%  to-mauve1A via-transparent"
             borderGradeintFrom="from-violet4"
+            glowColor="bg-violet3A"
+            glowBlur="blur-90"
+            glowOpacity="opacity-30"
             gridPosition="g-col-1/4 g-row-1/3 xxs:g-col-1/4 xxs:g-row-1/4"
             src="/tool-logos/nextjs.svg"
             alt="NEXTjs"
@@ -83,8 +87,11 @@ export default function Tools() {
             glowingCategory={glowingCategory}
           />
           <Li
-            className=" bg-gradient-to-b from-sky1 to-sky3 "
+            bgGradient=" bg-gradient-to-br from-transparent   to-sky2A  "
             borderGradeintFrom="from-sky4A"
+            glowColor="bg-sky3A"
+            glowBlur="blur-70"
+            glowOpacity="opacity-40"
             gridPosition="g-col-3/5 g-row-3/5  xxs:g-col-4/6 xxs:g-row-1/3"
             src="/tool-logos/react.svg"
             alt="React"
@@ -93,8 +100,11 @@ export default function Tools() {
             glowingCategory={glowingCategory}
           />
           <Li
-            className="  bg-gradient-to-b from-transparent via-transparent  to-mint2A"
+            bgGradient="  bg-gradient-to-b from-transparent via-transparent  to-mint2A"
             borderGradeintFrom="from-green3A"
+            glowColor="bg-mint3A"
+            glowBlur="blur-80"
+            glowOpacity="opacity-60"
             gridPosition="g-col-2/5 g-row-7/8  xxs:g-col-6/9 xxs:g-row-1/2"
             src="/tool-logos/supabase.svg"
             size="w-7/10"
@@ -109,8 +119,9 @@ export default function Tools() {
             alt="Progressive Web Applications"
           /> */}
           <Li
-            className=" bg-gradient-to-b from-mauve1A via-mauve1 to-mauve2A c-gold12 "
+            bgGradient=" bg-gradient-to-b from-mauve1A via-mauve1 to-mauve2A "
             borderGradeintFrom="from-gold4A"
+            glowColor="bg-gold3A"
             gridPosition="g-col-1/3 g-row-4/5    xxs:g-col-4/6 xxs:g-row-3/4"
             src="/tool-logos/radix-ui.svg"
             size="w-6/10 filter-invert"
@@ -118,8 +129,9 @@ export default function Tools() {
             glowingCategory={glowingCategory}
           />
           <Li
-            className="bg-gradient-to-b from-bronze1A via-orange1A via-55% to-orange3A "
+            bgGradient="bg-gradient-to-b from-bronze1A via-orange1A via-55% to-orange3A "
             borderGradeintFrom="from-amber3A"
+            glowColor="bg-amber3A"
             gridPosition="g-col-4/5 g-row-1/3  xxs:g-col-6/7 xxs:g-row-2/4"
             src="/tool-logos/astrojs.svg"
             alt="Astro"
@@ -127,8 +139,12 @@ export default function Tools() {
             glowingCategory={glowingCategory}
           />
           <Li
-            className="  bg-gradient-to-b from-transparent via-transparent  to-lime2A  lt-xxs:a "
+            className="lt-xxs:a"
+            bgGradient="bg-gradient-to-b from-transparent from-30%  to-lime3A "
             borderGradeintFrom="from-lime4A"
+            glowColor="bg-lime3A"
+            glowBlur="blur-50"
+            glowOpacity='opacity-50'
             gridPosition="g-col-2/3 g-row-3/4  xxs:g-col-7/9 xxs:g-row-2/4 "
             src="/tool-logos/nodejs.svg"
             alt="NodeJS"
@@ -138,8 +154,11 @@ export default function Tools() {
           />
 
           <Li
-            className=" dark bg-gradient-to-b from-sand1A via-sand1A to-bronze2A"
+            bgGradient=" dark bg-gradient-to-b from-sand1A via-sand1A to-bronze2A"
             borderGradeintFrom="from-bronze5A"
+            glowColor="bg-ruby3A"
+            glowBlur="blur-90"
+            glowOpacity='opacity-30'
             gridPosition="g-col-1/4 g-row-5/7  xxs:g-col-4/7 xxs:g-row-4/6"
             src="/tool-logos/figma.svg"
             alt="Figma"
@@ -150,8 +169,10 @@ export default function Tools() {
 
           {/* <Li className="bg-violet-100" src="/tool-logos/framer-motion.png" size="w-9/10" alt="Framer Motion" /> */}
           <Li
-            className="col-span-2 bg-gradient-to-b from-transparent via-transparent to-bronze2 !hidden xxs:!flex justify-center"
+            className="!hidden xxs:!flex col-span-2 justify-center"
+            bgGradient=" bg-gradient-to-b from-transparent via-transparent to-bronze2 "
             borderGradeintFrom="from-sand4A"
+            glowColor="bg-sand3A"
             gridPosition="  xxs:g-col-7/9 xxs:g-row-4/5"
             src="/tool-logos/unocss.svg"
             alt="UnoCSS"
@@ -159,8 +180,10 @@ export default function Tools() {
           />
 
           <Li
-            className="col-span-2 bg-gradient-to-b from-cyan2A to-cyan3A"
+            className="col-span-2"
+            bgGradient=" bg-gradient-to-b from-cyan2A to-cyan3A"
             borderGradeintFrom="from-cyan4A"
+            glowColor="bg-cyan3A"
             gridPosition="g-col-3/5 g-row-8/9  xxs:g-col-2/4 xxs:g-row-5/6"
             src="/tool-logos/tailwindcss.svg"
             alt="TailwindCSS"
@@ -169,8 +192,11 @@ export default function Tools() {
             glowingCategory={glowingCategory}
           />
           <Li
-            className="bg-gradient-to-b from-transparent via-transparent to-blue1A  "
+            bgGradient="bg-gradient-to-b from-transparent via-transparent to-blue1A  "
             borderGradeintFrom="from-sky4A"
+            glowColor="bg-sky3A"
+            glowBlur="blur-50"
+            glowOpacity="opacity-80"
             gridPosition="g-col-1/3 g-row-8/9  xxs:g-col-1/3 xxs:g-row-4/5"
             src="/tool-logos/solidjs.svg"
             size="w-8/10"
@@ -199,8 +225,9 @@ export default function Tools() {
           /> */}
 
           <Li
-            className="bg-gradient-to-b from-purple1 to-purple4  "
+            bgGradient="bg-gradient-to-b from-purple1 to-purple4  "
             borderGradeintFrom="from-purple5A"
+            glowColor="bg-purple3A"
             gridPosition="g-col-1/2 g-row-7/8  xxs:g-col-1/2 xxs:g-row-5/6"
             src="/tool-logos/vite.svg"
             alt="Vite"
@@ -212,8 +239,9 @@ export default function Tools() {
 
           {/* <Li className="bg-sky-50" icon="i-logos-postgresql" size="!w-2em !h-2em" alt="PostgreSQL" /> */}
           <Li
-            className="bg-gradient-to-b from-slate2A via-indigo3A to-indigo4A "
+            bgGradient="bg-gradient-to-b from-slate2A via-indigo3A to-indigo4A "
             borderGradeintFrom="from-indigo4A"
+            glowColor="bg-indigo3A"
             gridPosition="g-col-4/5 g-row-6/7  xxs:g-col-8/9 xxs:g-row-5/6"
             src="/tool-logos/css.svg"
             alt="CSS"
@@ -221,8 +249,9 @@ export default function Tools() {
             glowingCategory={glowingCategory}
           />
           <Li
-            className=" bg-gradient-to-b from-base2A via-orange3A to-orange4A"
+            bgGradient=" bg-gradient-to-b from-base2A via-orange3A to-orange4A"
             borderGradeintFrom="from-orange4A"
+            glowColor="bg-orange3A"
             gridPosition="g-col-4/5 g-row-5/6  xxs:g-col-7/8 xxs:g-row-5/6"
             src="/tool-logos/html.svg"
             alt="HTML"
@@ -230,8 +259,9 @@ export default function Tools() {
           />
 
           <Li
-            className=" bg-gradient-to-b from-sky1 via-sky2 to-sky3  "
+            bgGradient=" bg-gradient-to-b from-sky1 via-sky2 to-sky3  "
             borderGradeintFrom="from-sky4"
+            glowColor="bg-sky3A"
             gridPosition="g-col-1/2 g-row-3/4  xxs:g-col-3/4 xxs:g-row-4/5"
             src="/tool-logos/typescript.svg"
             alt="TypeScript"
@@ -250,12 +280,24 @@ export default function Tools() {
 function Li(props) {
   return (
     <li
-      className={`toolkit__grid-item empty:b-transparent bg-origin-border relative isolate ${props.className} ${
+      className={`toolkit__grid-item empty:b-transparent bg-origin-border relative isolate  ${props.className} ${
         props.gridPosition
       } 
-      ${props.glowingCategory && !props.isGlowing ? "grayscale-100" : ""}
+      ${props.glowingCategory && !props.isGlowing ? "grayscale-95" : "grayscale-0"}
+      transition-filter duration-1s ease
       `}
     >
+      <GlowOverlay
+        className={`-z-3 rd-inherit  ${props?.glowColor ?? "bg-base4A"} 
+        ${props.glowBlur ?? "blur-20"} mix-blend-overlay 
+        ${props.glowingCategory && props.isGlowing ? props.glowOpacity ?? "opacity-100": "opacity-0"}
+        transition-filter transition-opacity duration-1s ease`}
+      />
+      <BgOverlay className="bg-base1 opacity-30 rd-inherit -z-2 backdrop-blur-20" />
+      <BgOverlay className={` rd-inherit -z-1 bg-gradient-to-b ${props.bgGradient}`} style={{
+        "--un-gradient-shape": "135deg"
+      }}/>
+
       <GradientBorderLayer borderGradeintFrom={props?.borderGradeintFrom} />
       <div className="flex justify-center items-center p-4  xxs:!p-3 xs:p-4 sm:p-4 h-full">
         {props.icon && <Icon name={props.icon} className={`${props.size}`} />}
@@ -267,7 +309,7 @@ function Li(props) {
 
 function GradientBorderLayer({
   borderGradeintFrom = "from-slate4A",
-  borderGradeintTo = "salte3A",
+  borderGradeintTo = "to-slate3A",
   borderGradientVia = "via-slate2A",
 }) {
   return (
@@ -298,6 +340,37 @@ function GradientBorderLayer({
   );
 }
 
+function BgOverlay({ className, style = {} }) {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        ...style
+      }}
+      className={`${className}`}
+    ></div>
+  );
+}
+
+function GlowOverlay({ className }) {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+      }}
+      className={`${className}`}
+    ></div>
+  );
+}
+
 function ButtonWithToolTip({
   label,
   category,
@@ -322,10 +395,15 @@ function ButtonWithToolTip({
               setGlowingCategory(category);
             }
           }}
-          className={`!b-transparent relative !b-0 !rd-2 !bg-transparent  hover:bg-gradient-to-b from-transparent ${
-            glowingCategory === category ? "bg-gradient-to-b to-cyan3A" : " to-base3A"
-          }`}
+          className={`!b-transparent relative !b-0 !rd-2 !bg-transparent c-sage11   hover:bg-gradient-to-b from-transparent ${
+            glowingCategory === category ? "bg-gradient-to-b to-cyan3A !c-cyan9" : " to-cyan2A"
+          } transition ease duration-150ms`}
         >
+          <GlowOverlay
+            className={`-z-1 bg-cyan3A blur-20 mix-blend-overlay ${
+              glowingCategory === category ? "opacity-100" : "opacity-0"
+            }`}
+          />
           <GradientBorderLayer
             borderGradeintFrom={glowingCategory === category ? "from-accent5" : "from-base5"}
             borderGradientVia={glowingCategory === category ? "from-accent5" : "from-base5"}
--- components/ui/Tooltip/index.tsx
+++ components/ui/Tooltip/index.tsx
@@ -5,44 +5,85 @@ import styles from "./styles.module.css";
 export default function ToolTip({
   trigger,
   children,
-  open , setOpen
+  open,
+  setOpen,
 }: {
   trigger: React.ReactNode;
   children: React.ReactNode;
-  open ?: boolean,
-  setOpen?: (v: boolean) => void
+  open?: boolean;
+  setOpen?: (v: boolean) => void;
 }) {
   return (
-
     <RadixToolTip.Provider>
-    <RadixToolTip.Root 
-    open={open}
-    onOpenChange={(v) => {setOpen?.(v)}}
-    delayDuration={100}
-    >
-      <RadixToolTip.Trigger asChild>{trigger}</RadixToolTip.Trigger>
-      <RadixToolTip.Portal>
-
-        <RadixToolTip.Content
-          className={`
+      <RadixToolTip.Root
+        open={open}
+        onOpenChange={(v) => {
+          setOpen?.(v);
+        }}
+        delayDuration={0}
+      >
+        <RadixToolTip.Trigger asChild>{trigger}</RadixToolTip.Trigger>
+        <RadixToolTip.Portal>
+          <RadixToolTip.Content
+            className={`relative
             ${styles["TooltipContent"]}`}
             sideOffset={5}
+          >
+            <div
+              className="rd-3  p-4 relative
+               shadow-2xl shadow-black bg-base2A  backdrop-blur-10
+            "
             >
-            <div className='rd-3  p-4
-               shadow-xl bg-base3
-            '>
+              <GradientBorderLayer
+                borderGradeintFrom={"from-base3A"}
+                borderGradientVia={"via-transparent"}
+                borderGradeintTo={"to-transparent"}
+              />
 
-          {children}
-          <RadixToolTip.Arrow
-  /* padding: 1.25rem; */
-            className={`fill-base3 ${styles["TooltipArrow"]}`}
-            height={10}
-            width={20}
-            />
+              {children}
+              <RadixToolTip.Arrow
+                /* padding: 1.25rem; */
+                className={`fill-base2 ${styles["TooltipArrow"]}`}
+                height={10}
+                width={20}
+              />
             </div>
-        </RadixToolTip.Content>
-      </RadixToolTip.Portal>
-    </RadixToolTip.Root>
-            </RadixToolTip.Provider>
+          </RadixToolTip.Content>
+        </RadixToolTip.Portal>
+      </RadixToolTip.Root>
+    </RadixToolTip.Provider>
+  );
+}
+
+function GradientBorderLayer({
+  borderGradeintFrom = "from-slate4A",
+  borderGradeintTo = "to-slate3A",
+  borderGradientVia = "via-slate2A",
+}) {
+  return (
+    <div
+      style={{
+        // gridArea: "1/1/-1/-1",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        borderRadius: "inherit",
+        // overflow: 'clip',
+        // borderWidth: 'inherit',
+        borderStyle: "solid",
+        borderColor: "transparent",
+        backgroundClip: "border-box",
+        backgroundOrigin: "border-box",
+        WebkitMask: "linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0)",
+        mask: "linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0)",
+        WebkitMaskComposite: "xor" /*5'*/,
+        maskComposite: "exclude" /*5*/,
+        backdropFilter: "blur(5px)",
+        // zIndex: 9,
+      }}
+      className={` bg-gradient-to-b bg-base2A ${borderGradeintFrom} ${borderGradientVia} ${borderGradeintTo} b-1 mix-blend-screen `}
+    ></div>
   );
 }
