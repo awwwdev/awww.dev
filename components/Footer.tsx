c3db3132d8150d4459ca8240dde2bc23b846ae73
hamid <->
2024-08-29 00:39:24 -0700

    feat: add source code address.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,27 @@
+import Icon from "./ui/Icon";
 
 const Footer = () => {
-
   return (
     <>
       <div className="h-20"></div>
       <footer className="px-4 relative isolate ">
-      <div className=" absolute bottom-0 left-50% -translate-x-50%  w-full overflow-clip -z-10 pointer-events-none select-none">
-        <DotGrid rows={24} cols={24} />
-      </div>
+        <div className=" absolute bottom-0 left-50% -translate-x-50%  w-full overflow-clip -z-10 pointer-events-none select-none">
+          <DotGrid rows={24} cols={24} />
+        </div>
         <div className=" max-w-page mx-auto "></div>
         <div className="max-w-page mx-auto">
-          <div className="b-t-1 b-base4 mt-4 pt-8 pb-4 text-sm c-base11">
+          <div className="b-t-1 b-base4 mt-4 pt-8 pb-8 text-sm c-base11">
             <p>Designed and Developed with love, ofcourse by myself.</p>
             <p>No AI was used during building of this website!</p>
+            <p>
+              View the source code{` `}
+              <a target='_blank' href="https://github.com/awwwdev/awww.dev">
+                in my
+                <Icon name="bf-i-bxl-github" className="" />
+                GitHub
+              </a>
+              .
+            </p>
           </div>
         </div>
       </footer>
@@ -35,7 +44,11 @@ function DotGrid({ rows, cols }) {
       }}
     >
       {Array.from(Array(rows * cols).keys()).map((i) => {
-        return <div key={`dot-${i}`} className={`rd-full  w-1 h-1 `}>+</div>;
+        return (
+          <div key={`dot-${i}`} className={`rd-full  w-1 h-1 `}>
+            +
+          </div>
+        );
       })}
     </div>
   );
