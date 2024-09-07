1f44ce168261fb67afa674e06d909d8e5e24fbf9
hamid <->
2024-09-07 16:08:44 +0330

    fix: update About me

--- components/home-page-sections/AboutMe.tsx
+++ components/home-page-sections/AboutMe.tsx
@@ -1,26 +1,30 @@
-import Space from '../ui/Space';
+import Space from "../ui/Space";
 import BluredCircle from "./BluredCircle";
 
 export default function AboutMe() {
-
   return (
     <section className="relative">
       <BluredCircle radius={150} top="85%" left="60%" bg="bg-gold2A" blur="200px" />
       <div className="mx-auto max-w-page  ">
-          <h2 className="H1" id='about'>About Me</h2>
+        <h2 className="H1" id="about">
+          About Me
+        </h2>
         <Space size="h-4" />
         <div className="space-y-4 c-gray11">
+          <p>I help businesses create user experiences that give them a competitive edge.</p>
+          <p>Over the past few years, I've worked with various teams as both a Developer and Designer.</p>
           <p>
-            I help businesses to discover their visual language, create wireframes, make high fidelity UI prototypes and develop it into production.
+            As a developer, I build everything from complex web applications to landing pages with bold designs,
+            ensuring top performance and accessibility. While I specialize in front-end development, I also design data
+            structures, set up REST APIs, and create backend applications using modern web technologies like tRPC,
+            PostgreSQL, and Node.js.
           </p>
-          <p>During past years, I have worked with many teams as a Designer, Developer or both.</p>
           <p>
-            My experinces are most beneficial to competitons who want to stand apart from the competition, by delivering
-            astonishing User Interfaces, but don&apos;t want to comprimise performance, accessiblity and development speed.
+            As a designer, I collaborate with teams to define their visual identity, create wireframes, and develop
+            high-fidelity UI prototypes ready for development.
           </p>
           <p>
-            Besides work, I spend my time with illustrating, Photography and Music. I&apos;d love to explore areas that
-            combine Art and Web Technologies together!
+            Outside of work, I enjoy drawing, photography, and playing music. I'm always excited to explore new ways to combine art and web technologies.
           </p>
         </div>
       </div>
